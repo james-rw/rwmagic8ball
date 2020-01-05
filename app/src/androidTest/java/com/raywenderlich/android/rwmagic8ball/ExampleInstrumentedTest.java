6280174f1b06704741dbf931039a1746ca6ff1d2
James Nocentini <james.nocentini.rw@gmail.com>
2020-01-05 13:05:45 +0000

    remove code comment

--- app/src/androidTest/java/com/raywenderlich/android/rwmagic8ball/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/raywenderlich/android/rwmagic8ball/ExampleInstrumentedTest.java
@@ -46,11 +46,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(JUnit4.class)
 public class ExampleInstrumentedTest {
 
